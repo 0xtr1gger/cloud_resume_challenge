8c16971eb68ce65f99a15a72d2c50687f6dde3b2
0xtr1gger <w1lliamjames@tutamail.com>
2025-03-08 16:51:32 +0200

    fix ansible roles

--- examples/api_vm/providers.tf
+++ examples/api_vm/providers.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">=1.0"
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = ">= 2.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
@@ -16,11 +12,6 @@ terraform {
     container_name       = "terraform-state"
     key                  = "terraform.vm_tfstate"
   }
-}
-
-
-provider "azapi" {
-
 }
 
 provider "azurerm" {
